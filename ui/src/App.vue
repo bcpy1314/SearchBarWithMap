4c0f7fcebe2c85fcbaf132dc5beea40dd8313fbd
bcpy1314 <panyueabc@gmail.com>
2022-07-03 10:09:18 -0700

    searchbarwithmap

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <GoogleMap />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import GoogleMap from "./components/GoogleMap.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    GoogleMap,
+  },
+};
 </script>
 
 <style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGoogleMaps from "vue3-google-map";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyC0_7qBQT6uvcgQ_zqcnO8KeFufZhbCDCo",
+    },
+  })
+  .mount("#app");
